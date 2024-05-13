cbdec36107863593b2d0f16a02a6e6f147b75b4e
Dominik <dominik.galczyk@gmail.com>
2024-05-13 17:11:04 +0200

    added functionality to todo list

--- app/src/main/java/com/example/projectair/MainApp.kt
+++ app/src/main/java/com/example/projectair/MainApp.kt
@@ -3,16 +3,20 @@ package com.example.projectair
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainApp : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private lateinit var todoAdapter: TodoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +41,28 @@ class MainApp : AppCompatActivity() {
             val intent = Intent(this, Login::class.java)
             startActivity(intent)
             finish()
+        }
+
+        todoAdapter = TodoAdapter(mutableListOf())
+        val todoItemsRV = findViewById<RecyclerView>(R.id.rVTodoItems)
+        todoItemsRV.adapter = todoAdapter
+        todoItemsRV.layoutManager = LinearLayoutManager(this)
+
+        val buttonAdd = findViewById<Button>(R.id.btnAddTodo)
+        val buttonDelete = findViewById<Button>(R.id.btnDeleteDoneTodos)
+        val textEditTitle = findViewById<EditText>(R.id.eTTodoTitle)
+
+        buttonAdd.setOnClickListener {
+            val todoTitle = textEditTitle.text.toString()
+            if (todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                textEditTitle.text.clear()
+            }
+        }
+
+        buttonDelete.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
         }
     }
 }
---
+++ app/src/main/java/com/example/projectair/Todo.kt
@@ -0,0 +1,6 @@
+package com.example.projectair
+
+data class Todo(
+    val title: String,
+    var isChecked: Boolean = false
+)
